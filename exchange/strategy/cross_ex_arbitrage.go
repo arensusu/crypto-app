a4013139f1c828ad689cc99c0381e99631c92486
arensusu <willy9966@gmail.com>
2023-10-28 08:49:20 +0000

    update cross exchange

--- exchange/strategy/cross_ex_arbitrage.go
+++ exchange/strategy/cross_ex_arbitrage.go
@@ -1,5 +1,13 @@
 package strategy
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"sync"
+	"time"
+)
+
 type CrossExArbitrageInformation struct {
 	ExchangeName    string
 	Symbol          string
@@ -22,6 +30,104 @@ type FundingPriceDiff struct {
 
 type SymbolFundingPriceDiffs map[string][]FundingPriceDiff
 
-type CrossExArbitrageUsecase interface {
-	CalculateCrossExArbitrage() (SymbolFundingPriceDiffs, error)
+func (exs *StrategyExecuter) GetCrossExchangeArbitrage() {
+	//start := time.Now().UnixMilli()
+
+	wg := new(sync.WaitGroup)
+	wg.Add(len(exs.Exchanges))
+
+	lock := new(sync.Mutex)
+	result := SymbolExchangeFundingPrice{}
+	_ = result
+	for _, ex := range exs.Exchanges {
+		go func(ex any) {
+			defer wg.Done()
+			strat, ok := ex.(CrossExArbitrager)
+			if !ok {
+				log.Fatal(fmt.Errorf("type error: %v", ex))
+			}
+
+			info, err := (strat).GetCrossExArbitrageInformation()
+			if err != nil {
+				log.Fatal(err)
+			}
+			lock.Lock()
+			for k, v := range info {
+				result[k] = append(result[k], v...)
+			}
+			lock.Unlock()
+		}(ex)
+	}
+	wg.Wait()
+	fmt.Println(result["TRBUSDT"])
+
+	results := SymbolFundingPriceDiffs{}
+	for symbol, fundingPrices := range result {
+
+		results[symbol] = calculateFundingPrices(fundingPrices)
+	}
+	fmt.Println(results["TRBUSDT"])
+}
+
+func calculateFundingPrices(fps []FundingPrice) []FundingPriceDiff {
+	diffs := []FundingPriceDiff{}
+	for i := 0; i < len(fps); i += 1 {
+		for j := i + 1; j < len(fps); j += 1 {
+			diffs = append(diffs, getBestCrossExchangeBuySell(&fps[i], &fps[j]))
+		}
+	}
+	return diffs
+}
+
+func getBestCrossExchangeBuySell(fpA, fpB *FundingPrice) FundingPriceDiff {
+	var buy, sell *FundingPrice
+
+	if isFundingTimeNotTheSame(fpA, fpB) {
+		buy, sell = getBuySellByFundingTime(fpA, fpB)
+	} else if isPriceDiffLarger(fpA, fpB) {
+		buy, sell = getBuySellByPrice(fpA, fpB)
+	} else {
+		buy, sell = getBuySellByFundingRate(fpA, fpB)
+	}
+
+	diff := FundingPriceDiff{
+		ExchangeBuy:     buy.Exchange,
+		ExchangeSell:    sell.Exchange,
+		PriceDiff:       buy.Price / sell.Price,
+		FundingRateDiff: buy.FundingRate - sell.FundingRate,
+		FundingTime:     time.UnixMilli(buy.FundingTime).In(time.FixedZone("UTC+8", 8*60*60)).Format("2006/01/02 15:04 MST"),
+	}
+
+	return diff
+}
+
+func isFundingTimeNotTheSame(fpA, fpB *FundingPrice) bool {
+	return fpA.FundingTime != fpB.FundingTime && fpA.FundingTime != 0 && fpB.FundingTime != 0
+}
+
+func getBuySellByFundingTime(fpA, fpB *FundingPrice) (*FundingPrice, *FundingPrice) {
+	if fpA.FundingTime <= fpB.FundingTime {
+		return fpA, fpB
+	}
+	return fpB, fpA
+}
+
+func isPriceDiffLarger(fpA, fpB *FundingPrice) bool {
+	priceDiff := fpA.Price/fpB.Price - 1
+	fundingRateDiff := fpA.FundingRate - fpB.FundingRate
+	return math.Abs(priceDiff) > math.Abs(fundingRateDiff)
+}
+
+func getBuySellByPrice(fpA, fpB *FundingPrice) (*FundingPrice, *FundingPrice) {
+	if fpA.Price <= fpB.Price {
+		return fpA, fpB
+	}
+	return fpB, fpA
+}
+
+func getBuySellByFundingRate(fpA, fpB *FundingPrice) (*FundingPrice, *FundingPrice) {
+	if fpA.FundingRate <= fpB.FundingRate {
+		return fpA, fpB
+	}
+	return fpB, fpA
 }
--- exchange/strategy/strategy.go
+++ exchange/strategy/strategy.go
@@ -1,11 +1,7 @@
 package strategy
 
 import (
-	"fmt"
-	"log"
 	"strconv"
-	"sync"
-	"time"
 )
 
 type StrategyExecuter struct {
@@ -15,56 +11,6 @@ type StrategyExecuter struct {
 func New(exchanges ...interface{}) *StrategyExecuter {
 	ex := &StrategyExecuter{Exchanges: exchanges}
 	return ex
-}
-
-func (exs *StrategyExecuter) GetCrossExchangeArbitrage() {
-	//start := time.Now().UnixMilli()
-
-	wg := new(sync.WaitGroup)
-	wg.Add(len(exs.Exchanges))
-
-	lock := new(sync.Mutex)
-	result := SymbolExchangeFundingPrice{}
-	_ = result
-	for _, ex := range exs.Exchanges {
-		go func(ex any) {
-			defer wg.Done()
-			strat, ok := ex.(CrossExArbitrager)
-			if !ok {
-				log.Fatal(fmt.Errorf("type error: %v", ex))
-			}
-
-			info, err := (strat).GetCrossExArbitrageInformation()
-			if err != nil {
-				log.Fatal(err)
-			}
-			lock.Lock()
-			for k, v := range info {
-				result[k] = append(result[k], v...)
-			}
-			lock.Unlock()
-		}(ex)
-	}
-	wg.Wait()
-	fmt.Println(result["STRAXUSDT"])
-
-	results := SymbolFundingPriceDiffs{}
-	for symbol, fundingPrices := range result {
-		diffs := []FundingPriceDiff{}
-		for i := 0; i < len(fundingPrices); i += 1 {
-			for j := i + 1; j < len(fundingPrices); j += 1 {
-				diffs = append(diffs, FundingPriceDiff{
-					ExchangeBuy:     fundingPrices[j].Exchange,
-					ExchangeSell:    fundingPrices[i].Exchange,
-					PriceDiff:       rounding(fundingPrices[j].Price/fundingPrices[i].Price - 1.0),
-					FundingRateDiff: rounding(fundingPrices[j].FundingRate - fundingPrices[i].FundingRate),
-					FundingTime:     time.UnixMilli(fundingPrices[j].FundingTime).String() + "/" + time.UnixMilli(fundingPrices[i].FundingTime).String(),
-				})
-			}
-		}
-		results[symbol] = diffs
-	}
-	fmt.Println(results["STRAXUSDT"])
 }
 
 type FundingPrice struct {
@@ -105,11 +51,4 @@ func (m SymbolExchangeFundingPrice) SetSpecial(exchange, symbol, fundingRate, fu
 			}
 		}
 	}
-}
-
-func rounding(x float64) float64 {
-	if x < 0.0001 && x > -0.0001 {
-		return 0.0
-	}
-	return x
 }
